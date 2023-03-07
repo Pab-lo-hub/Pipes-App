2d29306c60cd5159c315c663c81e2250a1a4fa5c
Pab-lo-hub <salaberry.pablo@gmail.com>
2023-03-07 10:26:20 -0300

    Styles and PrimeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  nombre: string = 'Pablooooooo';
+  calor: number = 1000;
+  obj = {
+    nombre: "Pablo"
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
 }
